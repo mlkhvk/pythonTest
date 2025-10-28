17cf3af74041ecf4ff6096b964089985ee08a00c
mlkhvk <mlkhvkate@gmail.com>
2025-10-28 14:42:46 +0300

    Исправили pylint

--- test_calculator.py
+++ test_calculator.py
@@ -7,41 +7,53 @@ def test_calculator_functions():
     tests = [
         (
             "3 + 4 * 2",
-         ["3", "4", "2", "*", "+"],
+            [
+            "3", "4", "2", "*", "+"
+            ],
          11.0
         ),
         (
             "(3 + 4) * 2",
-         ["3", "4", "+", "2", "*"],
+            [
+            "3", "4", "+", "2", "*"
+            ],
          14.0
         ),
         (
             "10 + 2 * 6",
-         ["10", "2", "6", "*", "+"],
+            [
+            "10", "2", "6", "*", "+"
+            ],
          22.0
         ),
         (
             "100 * 2 + 12",
-         ["100", "2", "*", "12", "+"],
+            [
+            "100", "2", "*", "12", "+"
+            ],
          212.0
         ),
         (
             "100 * ( 2 + 12 )",
-         ["100", "2", "12", "+", "*"],
+            [
+            "100", "2", "12", "+", "*"
+            ],
          1400.0
         ),
         (
             "100 * ( 2 + 12 ) / 14",
-         ["100", "2", "12", "+", "*", "14", "/"],
+            [
+            "100", "2", "12", "+", "*", "14", "/"
+            ],
          100.0
         )
     ]
 
     for expr, expected_rpn, expected_result in tests:
         rpn = infix_to_rpn(expr)
-        assert rpn == expected_rpn, f"Ожидалось {expected_rpn}, получено {rpn}"
+        assert rpn == expected_rpn, f"Ожидалось {expected_rpn}, но {rpn}"
         result = evaluate_rpn(rpn)
-        assert abs(result - expected_result) < 1e-9, f"Ожидалось {expected_result}, получено {result}"
+        assert abs(result - expected_result) < 1e-9, f"Ожидалось {expected_result}, но {result}"
 
 if __name__ == "__main__":
     test_calculator_functions()
